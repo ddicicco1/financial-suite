7f3029464cccf5412a7df34635be6d658e553731
Goose Assistant <goose@example.com>
2025-04-14 11:47:57 -0700

    Add initial React application files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Financial Suite</h1>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
